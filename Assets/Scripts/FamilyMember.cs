d11e66d70fd420c7b572d6d59312a7dd9b1f2b5d
samir2901 <thunder.cse2018@gmail.com>
2020-05-15 20:09:50 +0530

    Update

--- Assets/Scripts/FamilyMember.cs
+++ Assets/Scripts/FamilyMember.cs
@@ -1,19 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FamilyMember : MonoBehaviour
 {
     public int maxHealth;
     public int currentHealth;
+    public Slider healthBar;
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+        healthBar.value = maxHealth;
     }
 
     private void Update()
     {
-        Debug.Log(currentHealth);
+        //Debug.Log(currentHealth);
+        healthBar.value = currentHealth;
     }
 }
--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -7,6 +7,12 @@ public class HealthBar : MonoBehaviour
 {
     public Slider healthBar;
     
+    public void setMaxHealth (int health)
+    {
+        healthBar.maxValue = health;
+        healthBar.value = health;
+    }
+
     public void setHealth(int health)
     {
         healthBar.value = health;
